34bf2b6fbad164c1d8c8ebb50397d2b7c888d381
Yao987654321 <13006493780@qq.com>
2020-10-26 21:01:52 +0800

    2020-10-26

---
+++ src/main/java/com/sdxb/blog/MybolgApplication.java
@@ -0,0 +1,13 @@
+package com.sdxb.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybolgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybolgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdxb/blog/MybolgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdxb.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybolgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
